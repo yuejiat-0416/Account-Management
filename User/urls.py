6d6ffd67bda617b99a9c06875970c5e4c434dccd
zuchuandatou <zaclieI56@gmail.com>
2023-08-07 16:52:00 -0400

    try to solve confilct

--- User/urls.py
+++ User/urls.py
@@ -3,5 +3,4 @@ from .views import dashboard, CustomRegistrationView
 
 urlpatterns = [
     path('dashboard', dashboard, name='homepage'),
-    path('accounts/signup/', CustomRegistrationView.as_view(), name='custom_signup'),
 ]
