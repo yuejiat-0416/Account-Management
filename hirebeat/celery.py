9ee33acef56672e28fc5a2b18bd9c4c615bf30c4
zuchuandatou <zaclieI56@gmail.com>
2023-08-09 02:07:39 -0400

    add celery.py

---
+++ hirebeat/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+
+app = Celery('your_project_name')
+
+# Load task modules from all registered Django apps.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load tasks from all registered apps
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
